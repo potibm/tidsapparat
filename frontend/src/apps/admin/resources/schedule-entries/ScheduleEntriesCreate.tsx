4b094241f014d2feb2f17800aa0bb74599b47ba8
poti <poti@kaoz.org>
2026-05-11 18:47:23 +0200

    fix: update default values in ScheduleEntriesCreate form

--- frontend/src/apps/admin/resources/schedule-entries/ScheduleEntriesCreate.tsx
+++ frontend/src/apps/admin/resources/schedule-entries/ScheduleEntriesCreate.tsx
@@ -5,14 +5,21 @@ import { useAppConfig } from "@core/config/useConfig";
 import { ScheduleFormData } from "./schedule_entry.types";
 
 export const ScheduleEntriesCreate = () => {
-  const { timezone } = useAppConfig();
+  const { timezone, event_durations } = useAppConfig();
 
   const transform = (data: ScheduleFormData) =>
     transformScheduleToAPI(data, timezone);
 
   return (
     <Create title="Add Event" transform={transform}>
-      <SimpleForm defaultValues={{ source: "*", type: "username" }}>
+      <SimpleForm
+        defaultValues={{
+          source: "*",
+          type: "username",
+          start_time_only: "15:00",
+          duration_mins: event_durations?.[0] ?? 60,
+        }}
+      >
         <ScheduleEntriesInputs />
       </SimpleForm>
     </Create>
