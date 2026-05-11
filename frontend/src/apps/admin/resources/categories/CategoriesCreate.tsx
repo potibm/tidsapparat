9132b87ebdda7f5170c96198cb84d9791a165cc5
poti <poti@kaoz.org>
2026-05-11 18:38:40 +0200

    fix: colors with suggestions

--- frontend/src/apps/admin/resources/categories/CategoriesCreate.tsx
+++ frontend/src/apps/admin/resources/categories/CategoriesCreate.tsx
@@ -1,10 +1,46 @@
 import { Create, SimpleForm, TextInput } from "react-admin";
+import { useFormContext } from "react-hook-form";
+import { InputAdornment, IconButton } from "@mui/material";
+import AutorenewIcon from "@mui/icons-material/Autorenew";
+import randomColor from "randomcolor";
+
+const getRandomColor = () => {
+  return randomColor({
+    luminosity: "bright",
+    format: "hex",
+  });
+};
+
+const RandomColorButton = () => {
+  const { setValue } = useFormContext();
+
+  const handleRandomize = () => {
+    setValue("color", getRandomColor(), { shouldDirty: true });
+  };
+
+  return (
+    <InputAdornment position="end">
+      <IconButton onClick={handleRandomize} title="Random Color">
+        <AutorenewIcon />
+      </IconButton>
+    </InputAdornment>
+  );
+};
 
 export const CategoriesCreate = () => (
   <Create title="Add Category">
-    <SimpleForm defaultValues={{ color: "#BF00FF" }}>
+    <SimpleForm defaultValues={{ color: getRandomColor() }}>
       <TextInput source="name" />
-      <TextInput source="color" type="color" />
+      <TextInput
+        source="color"
+        type="color"
+        helperText="Pick a color or click the randomize button"
+        slotProps={{
+          input: {
+            endAdornment: <RandomColorButton />,
+          },
+        }}
+      />
     </SimpleForm>
   </Create>
 );
